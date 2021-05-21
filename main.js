f151b99d31c7541912ee98957bb51f62205450fc
Sidharth Ponram <sidponram@gmail.com>
2021-05-21 15:36:11 -0400

    new commit

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const fs = require('fs'); 
+//getting the commands from the scripts file
 const create = require('./scripts/create'); 
 const read = require('./scripts/read');
 const update = require('./scripts/update');  
+//packing it up for publication
 const package = { 
     create: create, 
    read: read, 
